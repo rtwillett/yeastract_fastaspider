472c4d3552f90926844385a386ba1b7ce2b5fdca
Ryan Willett <ryanwillett@ryans-mbp.home>
2020-03-28 18:08:12 -0400

    Annotated better comments

--- yeastract_fastaspider.py
+++ yeastract_fastaspider.py
@@ -2,10 +2,16 @@ import pandas as pd
 import requests
 
 def FASTA_requester(u):
+'''
+Extracts and returns the text from within the txt file the target URL.
+'''
     url = requests.post(u)
     return url.text
 
 def FASTA_parser(x):
+'''
+Takes the text returned by FASTA_requester() and returns the sysname, protein name and protein sequence for the protein. 
+'''
 
     fasta = FASTA_requester(x)
 
@@ -33,17 +39,18 @@ def FASTA_parser(x):
 
     return (sysname, protname, protein)
 
+# Extracting the list of protein names from a CSV file for which to scrape the entries.
 data = pd.read_csv("gene_data.csv", usecols=["proteinname"])
-
 data = data["proteinname"].tolist()
 
-prot_data = [] #
+# Building url and scraping the data from each webpage entry. 
+prot_data = []
 for g in data:
     url = f"http://www.yeastract.com/download.php?type=aminoacid&id={g}"
     d = FASTA_parser(url)
     prot_data.append(d)
     print(d)
 
+# Data export into a CSV
 df = pd.DataFrame(prot_data, columns=['sysname', 'proteinname', 'prot_sequence'])
-
 df.to_csv(r"/Users/willettr/NYCDSA/scrapy/yeastract/gene_seq.csv", index=None, header=True)

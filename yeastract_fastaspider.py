55bc6221d96c5d7042b0397abd0f0fd54db954e5
rtwillett <ryan.willett@gmail.com>
2020-10-21 08:14:46 -0400

    Changed indent levels on docstring

--- yeastract_fastaspider.py
+++ yeastract_fastaspider.py
@@ -2,16 +2,16 @@ import pandas as pd
 import requests
 
 def FASTA_requester(u):
-'''
-Extracts and returns the text from within the txt file the target URL.
-'''
+    '''
+    Extracts and returns the text from within the txt file the target URL.
+    '''
     url = requests.post(u)
     return url.text
 
 def FASTA_parser(x):
-'''
-Takes the text returned by FASTA_requester() and returns the sysname, protein name and protein sequence for the protein. 
-'''
+    '''
+    Takes the text returned by FASTA_requester() and returns the sysname, protein name and protein sequence for the protein.
+    '''
 
     fasta = FASTA_requester(x)
 
@@ -43,7 +43,7 @@ Takes the text returned by FASTA_requester() and returns the sysname, protein na
 data = pd.read_csv("gene_data.csv", usecols=["proteinname"])
 data = data["proteinname"].tolist()
 
-# Building url and scraping the data from each webpage entry. 
+# Building url and scraping the data from each webpage entry.
 prot_data = []
 for g in data:
     url = f"http://www.yeastract.com/download.php?type=aminoacid&id={g}"
@@ -53,4 +53,4 @@ for g in data:
 
 # Data export into a CSV
 df = pd.DataFrame(prot_data, columns=['sysname', 'proteinname', 'prot_sequence'])
-df.to_csv(r"/Users/willettr/NYCDSA/scrapy/yeastract/gene_seq.csv", index=None, header=True)
+df.to_csv(r"/Users/willettr/NYCDSA/scrapy/yeastract/gene_seq2.csv", index=None, header=True)
